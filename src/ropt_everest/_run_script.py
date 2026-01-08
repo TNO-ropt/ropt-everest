fc139c722b8fe426be376ad4c24dfa0c6a761c3f
Peter Verveer <pieter.verveer@tno.nl>
2026-01-08 15:59:07 +0100

    Update evaluator code

--- src/ropt_everest/_run_script.py
+++ src/ropt_everest/_run_script.py
@@ -7,14 +7,16 @@ from pathlib import Path
 from typing import TYPE_CHECKING
 
 from ropt.plugins.compute_step.base import ComputeStep
+from ropt.plugins.evaluator.base import Evaluator
 from ropt.workflow import create_evaluator
 
 if TYPE_CHECKING:
     from collections.abc import Callable
 
+    import numpy as np
+    from numpy.typing import NDArray
     from ropt.enums import ExitCode
-    from ropt.evaluator import EvaluatorCallback
-    from ropt.plugins.evaluator.base import Evaluator
+    from ropt.evaluator import EvaluatorCallback, EvaluatorContext, EvaluatorResult
 
 
 class EverestRunScriptComputeStep(ComputeStep):
@@ -42,10 +44,21 @@ class EverestRunScriptComputeStep(ComputeStep):
         return None
 
 
+class _Evaluator(Evaluator):
+    def __init__(self, *, callback: EvaluatorCallback) -> None:
+        super().__init__()
+        self._evaluator_callback = callback
+
+    def eval(
+        self, variables: NDArray[np.float64], context: EvaluatorContext
+    ) -> EvaluatorResult:
+        return self._evaluator_callback(variables, context)
+
+
 def _run_script(
     func: Callable[[Evaluator], ExitCode | None], evaluator: EvaluatorCallback
 ) -> ExitCode | None:
-    function_evaluator = create_evaluator("function_evaluator", callback=evaluator)
+    function_evaluator = _Evaluator(callback=evaluator)
     return func(
         create_evaluator("everest/cached_evaluator", evaluator=function_evaluator)
     )
